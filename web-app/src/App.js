7c5024e1285b77762a1fc142e9fa8d56d947fb10
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-09-03 14:21:42 -0400

    Initial commit

---
+++ web-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Home = () => {
+  return "This is an example of a compnent";
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
